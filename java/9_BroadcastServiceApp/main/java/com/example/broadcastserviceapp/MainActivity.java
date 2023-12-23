fa69191a37958df7330e79ff877dfe31f277f33f
david sureway <nwachukwudavid100@gmail.com>
2023-12-23 21:27:55 +0100

    eighth nineth project

---
+++ java/9_BroadcastServiceApp/main/java/com/example/broadcastserviceapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.broadcastserviceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Register Dynamically the BroadCast Receiver
+        IntentFilter intentFilter = new IntentFilter("android.intent.action.AIRPLANE_MODE");
+        AirplaneModeReceiver br = new AirplaneModeReceiver();
+        registerReceiver(br, intentFilter);
+    }
+}

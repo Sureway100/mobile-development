99561a491bb15bf27a9b0291519bc7a440598306
david sureway <nwachukwudavid100@gmail.com>
2023-12-26 08:46:57 +0100

    twelveth thirteenth project

---
+++ java/12_DataBindingApp/main/java/com/example/databindingapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.databindingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.databindingapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding activityMainBinding;
+//    private MyClickHandler myClickHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Data Binding with TextView
+        Person p1 = new Person("Jack","jack@gmail.com");
+
+        activityMainBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main);
+
+        activityMainBinding.setPerson(p1);
+
+
+        // Binding the Handler for Click Events
+//        myClickHandler = new MyClickHandler(this,activityMainBinding);
+//        activityMainBinding.setClickHandler(myClickHandler);
+    }
+}
---
+++ java/13_QuadraticApp/main/java/com/example/quadraticapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.quadraticapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.quadraticapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainBinding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_main
+        );
+
+        MyEquation equation = new MyEquation(mainBinding);
+
+        mainBinding.setMyequation(equation);
+
+
+    }
+}
